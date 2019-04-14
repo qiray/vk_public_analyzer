41cb4dd0646c4d80b598675415383f8ba37d8071
qiray <zotovyaa@mail.ru>
2019-04-14 23:50:46 +0300

    Months plot added

--- timing.py
+++ timing.py
@@ -11,6 +11,9 @@ def get_dateposts(name, data, data_range, xticks):
     y = [data.count(str(i)) for i in x]
     if xticks:
         plt.xticks(x, xticks, rotation=45)
+    else:
+        plt.xticks(rotation=45)
+    plt.subplots_adjust(bottom=0.15)
     plt.plot(x, y, marker='o')
     plt.savefig(OUTPUT_DIR + name)
     plt.close()
@@ -23,18 +26,20 @@ def dateposts(db):
     times = sorted([datetime.datetime.fromtimestamp(x[0]).strftime('%H') for x in posts]) #hours
     years = sorted([datetime.datetime.fromtimestamp(x[0]).strftime('%Y') for x in posts]) #years
     weekdays = sorted([datetime.datetime.fromtimestamp(x[0]).strftime('%a') for x in posts]) #weekdays
-    months = sorted([datetime.datetime.fromtimestamp(x[0]).strftime('%m') for x in posts]) #months
+    months = sorted([datetime.datetime.fromtimestamp(x[0]).strftime('%B') for x in posts]) #months
     # dates = sorted([datetime.datetime.fromtimestamp(x[0]).strftime('%Y.%m.%d') for x in posts]) #dates
     # print(dates)
 
-    years_names = sorted(list(set(years)))
-    get_dateposts('posts_years.png', years, years_names, None)
+    years_list = sorted(list(set(years)))
+    get_dateposts('posts_years.png', years, years_list, None)
 
-    # month_names = [calendar.month_name[i + 1] for i in range(12)] #TODO: fix
-    # get_dateposts('posts_months.png', months, sorted(list(set(months))), month_names)
+    sorter = [calendar.month_name[i + 1] for i in range(12)]
+    sorterIndex = dict(zip(sorter, range(len(sorter))))
+    month_names = sorted(list(set(months)), key=lambda m: sorterIndex[m])
+    get_dateposts('posts_months.png', months, month_names, None)
 
     sorter = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    sorterIndex = dict(zip(sorter,range(len(sorter))))
+    sorterIndex = dict(zip(sorter, range(len(sorter))))
     weekdays_names = sorted(list(set(weekdays)), key=lambda day: sorterIndex[day])
     get_dateposts('posts_weekdays.png', weekdays, weekdays_names, None)
 
