8c39864dc1b6d430b723ad4d7e53074328fc9f53
qiray <zotovyaa@mail.ru>
2019-04-17 18:00:53 +0300

    Multiple plots changes

--- timing.py
+++ timing.py
@@ -2,14 +2,39 @@
 import datetime
 import calendar
 
+from mpl_toolkits.axes_grid1 import host_subplot
+import mpl_toolkits.axisartist as AA
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
 from common_data import OUTPUT_DIR
 
-def get_dateposts(name, y_func, data, data_range, xticks=None, autolocator=False):
+def get_dateposts(name, data, data_range, xticks=None, autolocator=False):
+    #https://matplotlib.org/examples/axes_grid/demo_parasite_axes2.html
     x = data_range
-    y = [y_func(data, str(i)) for i in x]
+    y1 = [posts_count(data, str(i)) for i in x] #posts
+    y2 = [get_count(data, str(i), 1) for i in x] #likes
+    y3 = [get_count(data, str(i), 2) for i in x] #reposts
+    # y4 = [get_count(data, str(i), 3) for i in x] #comments
+    # y5 = [get_count(data, str(i), 4) for i in x] #views
+    # y6 = [get_count(data, str(i), 5) for i in x] #attachments
+    # y7 = [get_count(data, str(i), 6) for i in x] #text length
+
+    host = host_subplot(111, axes_class=AA.Axes)
+    plt.subplots_adjust(right=0.75, bottom=0.15)
+
+    par1 = host.twinx()
+    par2 = host.twinx()
+
+    new_fixed_axis = par2.get_grid_helper().new_fixed_axis
+    par1.axis["right"] = new_fixed_axis(loc="right", axes=par1)
+    par2.axis["right"] = new_fixed_axis(loc="right", axes=par2, offset=(60, 0))
+
+    par2.axis["right"].toggle(all=True)
+    host.set_ylabel("posts")
+    par1.set_ylabel("likes")
+    par2.set_ylabel("reposts")
+
     if xticks:
         plt.xticks(x, xticks, rotation=45)
     else:
@@ -17,8 +42,25 @@ def get_dateposts(name, y_func, data, data_range, xticks=None, autolocator=False
     if autolocator:
         plt.gca().xaxis.set_major_locator(ticker.AutoLocator())
         plt.gca().xaxis.set_minor_locator(ticker.AutoMinorLocator())
-    plt.subplots_adjust(bottom=0.15)
-    plt.plot(x, y, marker='o')
+
+    p1, = host.plot(x, y1, marker='o', label='posts')
+    p2, = par1.plot([i for i in range(len(x))], y2, marker='o', label='likes')
+    p3, = par2.plot([i for i in range(len(x))], y3, marker='o', label='reposts')
+
+    host.axis["left"].label.set_color(p1.get_color())
+    par1.axis["right"].label.set_color(p2.get_color())
+    par2.axis["right"].label.set_color(p3.get_color())
+
+    host.legend()
+
+    # plt.plot(x, y1, marker='o', label='posts')
+    # plt.plot(x, y2, marker='o', label='likes')
+    # plt.plot(x, y3, marker='o', label='reposts')
+    # plt.plot(x, y4, marker='o', label='comments')
+    # plt.plot(x, y5, marker='o', label='views')
+    # plt.plot(x, y6, marker='o', label='attachments')
+    # plt.plot(x, y7, marker='o', label='text length')
+
     plt.savefig(OUTPUT_DIR + name)
     plt.close()
 
@@ -27,15 +69,10 @@ def posts_count(data, value):
         return len(data[value])
     return 0
 
-def likes_count(data, value):
+def get_count(data, value, index):
     if value in data:
-        return sum([x[1] for x in data[value]])
+        return sum([x[index] for x in data[value]])
     return 0
-
-def drawplots(db):
-    posts = db.get_posts_by_dates()
-    draw(posts, posts_count, 'posts')
-    draw(posts, likes_count, 'likes')
 
 def datalist_to_dict(data, converter):
     result = {}
@@ -47,10 +84,10 @@ def datalist_to_dict(data, converter):
             result[index].append(value)
     return result
 
-def draw(posts, func, name):
+def drawplots(db):
+    
+    posts = db.get_posts_by_dates()
     #TODO: add total info?
-    #TODO: all data on one plot
-    #TODO: add info about likes, reposts and comments and maybe wordcount
     
     years = datalist_to_dict(posts, lambda x: datetime.datetime.fromtimestamp(x[0]).strftime('%Y'))
     months = datalist_to_dict(posts, lambda x: datetime.datetime.fromtimestamp(x[0]).strftime('%B'))
@@ -59,23 +96,23 @@ def draw(posts, func, name):
     hours = datalist_to_dict(posts, lambda x: datetime.datetime.fromtimestamp(x[0]).strftime('%H'))
 
     years_list = sorted(years.keys())
-    get_dateposts(name + '_years.png', func, years, years_list)
+    get_dateposts('years.png', years, years_list)
 
     sorter = [calendar.month_name[i + 1] for i in range(12)]
     sorterIndex = dict(zip(sorter, range(len(sorter))))
     month_names = sorted(months.keys(), key=lambda m: sorterIndex[m])
-    get_dateposts(name + '_months.png', func, months, month_names)
+    get_dateposts('months.png', months, month_names)
 
     sorter = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
     sorterIndex = dict(zip(sorter, range(len(sorter))))
     weekdays_names = sorted(weekdays.keys(), key=lambda day: sorterIndex[day])
-    get_dateposts(name + '_weekdays.png', func, weekdays, weekdays_names)
+    get_dateposts('weekdays.png', weekdays, weekdays_names)
 
     d1 = datetime.datetime.strptime(min(dates), "%Y-%m").date()
     d2 = datetime.datetime.strptime(max(dates), "%Y-%m").date()
     delta = d2 - d1
     alldates = [(d1 + datetime.timedelta(i)).strftime('%Y-%m') for i in range(delta.days + 1)]
-    get_dateposts(name + '_allmonths.png', func, dates, alldates, autolocator=True)
+    get_dateposts('allmonths.png', dates, alldates, autolocator=True)
 
     hours_range = [i for i in range(24)]
-    get_dateposts(name + '_hours.png', func, hours, hours_range, ["%02d:00" % (i) for i in hours_range])
+    get_dateposts('hours.png', hours, hours_range, ["%02d:00" % (i) for i in hours_range])
